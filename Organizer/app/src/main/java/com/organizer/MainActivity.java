fb4d6f51c1ad76846b6db87a3858a1fc6f7836a3
iSvirchev <i.svirchev@yahoo.com>
2020-05-20 17:42:06 +0300

    Added a 2nd Activity screen 'Converter' and added a button on the main activity that opens the Converter activity.

--- Organizer/app/src/main/java/com/organizer/MainActivity.java
+++ Organizer/app/src/main/java/com/organizer/MainActivity.java
@@ -1,14 +1,33 @@
 package com.organizer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button converterActivityBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        converterActivityBtn = findViewById(R.id.converterActivityBtn);
+
+        converterActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openConverterActivity();
+            }
+        });
+    }
+
+    private void openConverterActivity() {
+        Intent intent = new Intent(this, Converter.class);
+        startActivity(intent);
     }
 }
