fc5cb11ee28648999b6ba30c8ad12435800b9f35
iSvirchev <i.svirchev@yahoo.com>
2020-06-16 13:42:12 +0300

    Created the new 'TimeConverter '

---
+++ Organizer/app/src/main/java/com/organizer/TimeConverter.java
@@ -0,0 +1,14 @@
+package com.organizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimeConverter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_converter);
+    }
+}
